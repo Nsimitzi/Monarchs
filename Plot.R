32d5b26ddfc9a2a0e25f7131560d793a143dac6e
Nicholas Simitzi <Nsimitzi@users.noreply.github.com>
2017-07-14 23:08:30 -0500

    Update Plot.R

--- Plot.R
+++ Plot.R
@@ -1,8 +1,7 @@
 library(gstat)
 library(maps)
 library(maptools)
-library(animation)
-library(plotly)
+
 
 #Read in Zipcode Shapefile
 ZipcodeFile <- readShapePoly("Zipcode Shapefiles(2015)/cb_2015_us_zcta510_500k.shp")
@@ -32,7 +31,6 @@ dat = dat[validDates, ]
 #presents in default format of xx/xx/xxxx
 dat$Date.Collected = as.Date(dat$Date.Collected[validDates], "%m/%d/%Y")
 
-
 #Finds unique zipcodes from the shapecode files
 UniqueZipShapeCode = unique(dat$zip)
 
@@ -45,8 +43,6 @@ updatedZipcodeFile = ZipcodeFile[SelectedZipCodes,]
 #Selects valid zipcodes from monarch data
 dat = dat[dat$zip %in% updatedZipcodeFile$ZCTA5CE10, ]
 
-
-
 #Subset monarch dataset to only include relevant categories
 monarch = dat
 monarch$status = as.numeric(monarch$status)
@@ -55,152 +51,13 @@ monarch$status = as.numeric(monarch$status)
 monarchZip = data.frame(zip = sort(unique(monarch$zip)))
 
 
-#Calculate butterflies caught per zipcode
-monarchZip$Captured = aggregate(status ~ zip, data = monarch, FUN = length)[, 2]
-
-#Calculate infected proportion per zipcode
-monarchZip$Proportion = aggregate(status ~ zip, data = monarch, FUN = mean)[, 2]
-
-#Calculate number of infected per zipcode
-monarchZip$Infected = aggregate(status ~ zip, data = monarch, FUN = function(x) sum(x))[, 2]
-
-
-
-pal = colorRampPalette(c("light gray", "red"))
-colors = pal(10)
-
-breaks = seq(0,max(monarchZip$Proportion),1/(length(colors)))
-
-
-val.levels = cut(monarchZip$Proportion, breaks,include.lowest = T)
-color.table = data.frame(level = levels(val.levels), color = I(colors))
-
-for(k in 1:length(updatedZipcodeFile$ZCTA5CE10)){
-  updatedZipcodeFile@data$color[k] = color.table$color[val.levels[k] == color.table$level]
-}
-
-
-
-plot(updatedZipcodeFile,col=updatedZipcodeFile$color,border=updatedZipcodeFile$color)
-
-map("state", add=T, col="gray")
-
-
-
-cen =  getSpPPolygonsLabptSlots(updatedZipcodeFile)
-data.f = data.frame(prop = monarchZip$Proportion ,cap = monarchZip$Captured ,inf = monarchZip$Infected)
-coordinates(data.f) = cen
-
-
-
-vgm <- variogram(prop~1, data.f)
-plot(vgm)
-fit <- fit.variogram(vgm, model=vgm(0.02, "Nug", 0))
-
-
-library(splancs)
-
-plot(data.f)
-
-
-st = map("state",interior = F)
-
-x = seq(min(st$x,na.rm = T),max(st$x,na.rm = T), length=200)
-y = seq(min(st$y,na.rm = T),max(st$y,na.rm = T), length=200)
-grd = expand.grid(x=x,y=y)
-coordinates(grd) <- ~ x+y
-
-
-
-id = idw(prop~1, data.f,grd)
-
-pred = as.data.frame(id)
-
-breaks = seq(0,max(pred[,3]),1/(length(colors)+1))
-val.levels = cut(pred[,3], breaks,include.lowest = T)
-color.table = data.frame(level = levels(val.levels), color = I(colors))
-
-
-for(k in 1:length(pred[,1])){
-  pred$color[k] = color.table$color[val.levels[k] == color.table$level]
-  cat(k,"\r")
-}
-
-plot(pred[,1],pred[,2], col= pred$color, pch=20)
-map("state", add=T, col="black")
-
-
-
-
-###################################
-#by year
-
-
+#plot per year by zipcode
 yr = format(as.Date(monarch$Date.Collected),"%Y")
 for(i.yr in 2011:2015){
   ind =  yr == i.yr
   
   mn2 = monarch[ind,]
   monarchZip = data.frame(zip = sort(unique(mn2$zip)) )
-  
-  
-  #Calculate butterflies caught per zipcode
-  monarchZip$Captured = aggregate(status ~ zip, data = mn2, FUN = length)[, 2]
-  
-  #Calculate infected proportion per zipcode
-  monarchZip$Proportion = aggregate(status ~ zip, data = mn2, FUN = mean)[, 2]
-  
-  #Calculate number of infected per zipcode
-  monarchZip$Infected = aggregate(status ~ zip, data = mn2, FUN = function(x) sum(x))[, 2]
-  
-  
-  #Finds unique zipcodes from the shapecode files
-  UniqueZipShapeCode = unique(mn2$zip)
-  
-  #Selects ZipCodes that are in both the shapecode and monarch data
-  SelectedZipCodes = ZipcodeFile$ZCTA5CE10 %in% UniqueZipShapeCode
-  
-  #ZipcodeFile with selected zipcodes
-  updatedZipcodeFile = ZipcodeFile[SelectedZipCodes,]
-  
-  
-  cen =  getSpPPolygonsLabptSlots(updatedZipcodeFile)
-  data.f = data.frame(prop = monarchZip$Proportion ,cap = monarchZip$Captured ,inf = monarchZip$Infected)
-  coordinates(data.f) = cen
-  
-  
-  id = idw(prop~1, data.f,grd)
-  
-  pred = as.data.frame(id)
-  
-  breaks = seq(0,max(pred[,3]),1/(length(colors)+1))
-  val.levels = cut(pred[,3], breaks,include.lowest = T)
-  color.table = data.frame(level = levels(val.levels), color = I(colors))
-  
-  
-  for(k in 1:length(pred[,1])){
-    pred$color[k] = color.table$color[val.levels[k] == color.table$level]
-    cat(k,"\r")
-  }
-  
-  plot(pred[,1],pred[,2], col= pred$color, pch=20, main = i.yr)
-  map("state", add=T, col="black")
-  
-  
-}
-
-
-
-
-
-######plot per year by zipcode
-yr = format(as.Date(monarch$Date.Collected),"%Y")
-for(i.yr in 2011:2015){
-  ind =  yr == i.yr
-  
-  mn2 = monarch[ind,]
-  monarchZip = data.frame(zip = sort(unique(mn2$zip)) )
-  
   
   #Calculate butterflies caught per zipcode
   monarchZip$Captured = aggregate(status ~ zip, data = mn2, FUN = length)[, 2]
@@ -219,15 +76,11 @@ for(i.yr in 2011:2015){
   
   #ZipcodeFile with selected zipcodes
   updatedZipcodeFile = ZipcodeFile[SelectedZipCodes,]
-  
-  data.f = data.frame(prop = monarchZip$Proportion ,cap = monarchZip$Captured ,inf = monarchZip$Infected, zip = monarchZip$zip)
-  
-  
+
   pal = colorRampPalette(c("light gray", "red"))
   colors = pal(10)
   
   breaks = seq(0,max(monarchZip$Proportion),1/(length(colors)))
-  
   
   val.levels = cut(monarchZip$Proportion, breaks,include.lowest = T)
   color.table = data.frame(level = levels(val.levels), color = I(colors))
@@ -236,10 +89,8 @@ for(i.yr in 2011:2015){
     updatedZipcodeFile@data$color[k] = color.table$color[val.levels[k] == color.table$level]
   }
   
-  ## plot(data.f$zip,col=updatedZipcodeFile$color,border=updatedZipcodeFile$color)
   plot(updatedZipcodeFile,col=updatedZipcodeFile$color,border=updatedZipcodeFile$color)
   map("state", add=T, col="black")
-  
   
 }
 
